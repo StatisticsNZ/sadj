0ec487acd3ccf18df1f3861a4eca89e9d1a49743
Steve White <stevemichaelwhite@gmail.com>
2021-11-11 16:49:57 +1300

    Fixed windows case-insensitivity stuff in findX13File.

--- R/utils2.R
+++ R/utils2.R
@@ -9,6 +9,7 @@ findX13File <- function(fname, rel_dir=getwd()){
 
 
   rel_base <- basename(rel_dir)
+  # rel_base <- rel_dir
 
   try_file <- sprintf("%s/%s",rel_dir,fname)
 
@@ -19,17 +20,40 @@ findX13File <- function(fname, rel_dir=getwd()){
 
   path_split <- stringr::str_split(fname, dir_delim)[[1]] %>% rev()
 
-  for(i in seq_along(path_split)) {
-    if(path_split[[i]]==rel_base) {
-      try_file <- paste(path_split[(i-1):1],collapse = "/") %>% sprintf("%s/%s",rel_dir,.)
-      if(file.exists(try_file)){
-        warning(sprintf("Couldn't find file:\n%s.\nFound file:\n%s.", fname
-                        , (found_file <- sub(path.expand("~"),"~",normalizePath(try_file)))))
-        return(found_file)
+  if(dir_delim == "\\\\") {
+
+    for(i in seq_along(path_split)) {
+      if(tolower(path_split[[i]])== tolower(rel_base)) {
+        end_bit <- paste(path_split[(i-1):1],collapse = "/")
+        end_bit <- rel_dir %>%
+          list.files(recursive = TRUE) %>%
+          grep(rex::rex(start, end_bit, end), . ,value=TRUE, ignore.case = TRUE)
+
+        try_file <- end_bit %>% sprintf("%s/%s",rel_dir,.)
+        if(file.exists(try_file)){
+          warning(sprintf("Couldn't find file:\n%s.\nFound file:\n%s.", fname
+                          , (found_file <- sub(path.expand("~"),"~",normalizePath(try_file)))))
+          return(found_file)
+        }
+      }
+    }
+
+  } else {
+
+    for(i in seq_along(path_split)) {
+      if(path_split[[i]]==rel_base) {
+        try_file <- paste(path_split[(i-1):1],collapse = "/") %>% sprintf("%s/%s",rel_dir,.)
+        if(file.exists(try_file)){
+          warning(sprintf("Couldn't find file:\n%s.\nFound file:\n%s.", fname
+                          , (found_file <- sub(path.expand("~"),"~",normalizePath(try_file)))))
+          return(found_file)
+        }
       }
     }
 
   }
+
+
 
   stop(sprintf("Unable to find file:\n%s.", fname))
 
